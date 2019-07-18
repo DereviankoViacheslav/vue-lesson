20b4d2d7b9ac7ed52cec9f5c4d2cdd430c4c373f
Derevianko Viacheslav <derevianko.viacheslav@gmail.com>
2019-07-18 06:36:06 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// let vm = 
+
+// new Vue({
+  // test: 'Hello World',
+//   render: h => h(App),
+// }).$mount('#app')
+
+// vm.data.test = 'Hello Vue';
+
+// let vm = new Vue({
+//   el: '#app2',
+//   data: {
+//     test: 'Hello Vue!'
+//   }
+// })
